d1fe994455b565ddb16ab6bcf9a36d8e44e123bc
leeyeji <leeyeji@msu.edu>
2024-10-09 20:12:21 -0400

    Upload New File

---
+++ project1Lib/Sparty.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file Sparty.cpp
+ * @author Yeji Lee
+ *
+ * implement the constructor, getter methods, and kick method
+ * kick: contains logic for kicking products off conveyot
+ */
+ 
+#include "Sparty.h"
+
+Sparty::Sparty(int x, int y, int height, wxPoint2DDouble pin, double kickDuration, double kickSpeed)
+    : mX(x), mY(y), mHeight(height), mPin(pin), mKickDuration(kickDuration), mKickSpeed(kickSpeed) {
+}
+
+int Sparty::GetX() const { return mX; }
+int Sparty::GetY() const { return mY; }
+double Sparty::GetKickSpeed() const { return mKickSpeed; }
+double Sparty::GetKickDuration() const { return mKickDuration; }
+
+void Sparty::Kick()
+{
+
+}
