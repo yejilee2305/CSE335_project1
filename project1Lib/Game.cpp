3f40dbda6527c77ff868e17a29cd7126592c5cd7
ZL <linzhi@msu.edu>
2024-10-12 22:18:02 -0400

    added a vector of items in game, and how item's constructor

--- project1Lib/Game.cpp
+++ project1Lib/Game.cpp
@@ -6,76 +6,90 @@
 #include "pch.h"
 #include "Game.h"
 
+/// initial item X location
+const int InitialX = 100;
+
+/// initial item Y location
+const int InitialY = 100;
+
+
 Game::Game() : currentLevel(0),
- score(0),
- gameOver(false),
- sparty(100, 200, 50, wxPoint2DDouble(0.0, 0.0), 1.5, 2.0),
- conveyor(0, 300, 5, 100, wxPoint(0,0)),
- sensor(50.0, 20.0)  // Pass double values for Sensor initialization
+               score(0),
+               gameOver(false),
+               sparty(this, 100, 200, 50, wxPoint2DDouble(0.0, 0.0), 1.5, 2.0),
+               conveyor(this, 0, 300, 5, 100, wxPoint(0, 0)),
+               sensor(this, 50.0, 20.0) // Pass double values for Sensor initialization
 {
- // resetting game
+    // resetting game
 }
 
 void Game::StartGame()
 {
- StartLevel(0);
+    StartLevel(0);
 }
 
 void Game::StartLevel(int levelNumber)
 {
-//starting certain level
+    //starting certain level
 }
 
 void Game::Update()
 {
- if (!gameOver)
- {
-  //conveyor.Update();
+    if (!gameOver)
+    {
+        //conveyor.Update();
 
-  //sensor.Update(conveyor.GetCurrentProduct());
-  //beam.Update(conveyor.GetCurrentProduct());
+        //sensor.Update(conveyor.GetCurrentProduct());
+        //beam.Update(conveyor.GetCurrentProduct());
 
-  //for (auto& gate : gates) {
-   //gate->Update();
-  }
+        //for (auto& gate : gates) {
+        //gate->Update();
+    }
 
-  //sparty.Update(beam.IsActive(), sensor.GetProductType());
+    //sparty.Update(beam.IsActive(), sensor.GetProductType());
 
-  //if (beam.IsActive() && sparty.ShouldKick())
-  {
-   //core += sparty.Kick(conveyor.GetCurrentProduct()) ? 10 : -5;
-  }
+    //if (beam.IsActive() && sparty.ShouldKick())
+    {
+        //core += sparty.Kick(conveyor.GetCurrentProduct()) ? 10 : -5;
+    }
 
-  //if (conveyor.IsFinished())
-  {
-   NextLevel();
-  }
- }
-
-
-void Game::ConnectGates(Gate* outputGate, Gate* inputGate) {
- if (outputGate && inputGate) {
-  //outputGate->ConnectTo(inputGate);
- }
+    //if (conveyor.IsFinished())
+    {
+        NextLevel();
+    }
 }
 
-int Game::GetScore() const {
- return score;
+
+void Game::ConnectGates(Gate* outputGate, Gate* inputGate)
+{
+    if (outputGate && inputGate)
+    {
+        //outputGate->ConnectTo(inputGate);
+    }
 }
 
-void Game::NextLevel() {
- currentLevel++;
- StartLevel(currentLevel);
+int Game::GetScore() const
+{
+    return score;
 }
 
-void Game::AddGate(Gate* gate) {
- if (gate) {
-  //gates.push_back(gate);  // 게이트 목록에 추가
- }
+void Game::NextLevel()
+{
+    currentLevel++;
+    StartLevel(currentLevel);
 }
 
-bool Game::IsGameOver() const {
- return gameOver;
+void Game::AddGate(Gate* gate)
+{
+    if (gate)
+    {
+        //gates.push_back(gate);  // 게이트 목록에 추가
+    }
+}
+
+bool Game::IsGameOver() const
+{
+    return gameOver;
 }
 
 // void Game::ResetGameState() {
@@ -92,28 +106,65 @@ bool Game::IsGameOver() const {
 
 void Game::OnDraw(std::shared_ptr<wxGraphicsContext> gc, int width, int height)
 {
- // Convert shared_ptr to raw pointer for wxGraphicsContext
- wxGraphicsContext* rawGc = gc.get();
+    // Convert shared_ptr to raw pointer for wxGraphicsContext
+    wxGraphicsContext* rawGc = gc.get();
 
- // Set the background to a specific color (you can adjust as necessary)
- rawGc->SetBrush(*wxBLACK_BRUSH);
- rawGc->DrawRectangle(0, 0, width, height);
+    // Set the background to a specific color (you can adjust as necessary)
+    rawGc->SetBrush(*wxBLACK_BRUSH);
+    rawGc->DrawRectangle(0, 0, width, height);
 
- // Draw the conveyor (assuming Conveyor has a Draw method)
- conveyor.Draw(rawGc);
+    // Draw the conveyor (assuming Conveyor has a Draw method)
+    conveyor.Draw(rawGc);
 
- // Draw Sparty (assuming Sparty has a Draw method)
- sparty.Draw(rawGc);
+    // Draw Sparty (assuming Sparty has a Draw method)
+    sparty.Draw(rawGc);
 
- // Draw products on the conveyor (assuming Product has a Draw method)
- for (const auto& product : conveyor.GetProducts())
- {
-  product->Draw(rawGc);
- }
+    // Draw products on the conveyor (assuming Product has a Draw method)
+    for (const auto& product : conveyor.GetProducts())
+    {
+        product->Draw(rawGc);
+    }
 
- // Optionally draw any gates or other game elements
- for (const auto& gate : gates)
- {
-  gate->Draw(rawGc);
- }
+    // Optionally draw any gates or other game elements
+    for (const auto& gate : gates)
+    {
+        gate->Draw(rawGc);
+    }
+}
+
+/**
+ * test x,y click location to see if it we clicked on a item
+ * @param x x location in pixel
+ * @param y y location in pixel
+ * @return pointer to the item we clicked on or nullptr if none
+ */
+std::shared_ptr<Item> Game::HitTest(int x, int y)
+{
+    for (auto i = mItems.begin(); i != mItems.end(); i++)
+    {
+        if ((*i)->HitTest(x, y))
+        {
+            return *i;
+        }
+    }
+
+    return nullptr;
+}
+
+/**
+ * deletes all the known items in the aquarium
+ */
+void Game::Clear()
+{
+    mItems.clear();
+}
+
+/**
+ * adds the item to the game
+ * @param item new item to add
+ */
+void Game::Add(std::shared_ptr<Item> item)
+{
+    item->SetLocation(InitialX, InitialY);
+    mItems.push_back(item);
 }
--- project1Lib/Game.h
+++ project1Lib/Game.h
@@ -4,7 +4,7 @@
  *
  *
  */
- 
+
 #ifndef GAME_H
 #define GAME_H
 
@@ -12,51 +12,62 @@
 #include "Gate.h"
 #include "Sparty.h"
 #include "Sensor.h"
-//#include "Beam.h"
+#include "Beam.h"
 #include "Conveyor.h"
+#include "Item.h"
 
-class Game {
+class Game
+{
 public:
- Game();
+    Game();
 
- // starting game
- void StartGame();
+    // starting game
+    void StartGame();
 
- // starting level ____
- void StartLevel(int Number);
+    // starting level ____
+    void StartLevel(int Number);
 
- //game update
- void Update();
+    //game update
+    void Update();
 
- // connecting gates
- void ConnectGates(Gate* outputGate, Gate* inputGate);
+    // connecting gates
+    void ConnectGates(Gate* outputGate, Gate* inputGate);
 
- // score
- int GetScore() const;
+    // score
+    int GetScore() const;
 
- // next level
- void NextLevel();
+    // next level
+    void NextLevel();
 
- // new gate
- void AddGate(Gate* gate);
+    // new gate
+    void AddGate(Gate* gate);
 
- // check if game has ended
- bool IsGameOver() const;
+    // check if game has ended
+    bool IsGameOver() const;
 
- void OnDraw(std::shared_ptr<wxGraphicsContext> gc, int width, int height);
+    void OnDraw(std::shared_ptr<wxGraphicsContext> gc, int width, int height);
+
+    void Add(std::shared_ptr<Item> item);
+
+    std::shared_ptr<Item> HitTest(int x, int y);
+
+    void Clear();
+
 
 private:
- int currentLevel;
- Sparty sparty;
- Conveyor conveyor;
- Sensor sensor;
- //Beam beam;
- //std::vector<Gate* gates>;
- int score;
- bool gameOver;
 
+    /// all of the items in our game
+    std::vector<std::shared_ptr<Item>> mItems;
+
+    int currentLevel;
+    Sparty sparty;
+    Conveyor conveyor;
+    Sensor sensor;
+    // Beam beam;
+    // std::vector<Gate* gates>;
+    int score;
+    bool gameOver;
 };
-
 
 
 #endif //GAME_H
