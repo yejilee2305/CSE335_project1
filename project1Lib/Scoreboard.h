63ed6f0b82ecab02e29c8493ebdb14e692ff9f42
Martin Manuel Sattam <sattamm1@msu.edu>
2024-10-18 16:19:58 -0400

    Updated Scoreboard so it appears for Level 1 with correct description.

--- project1Lib/Scoreboard.h
+++ project1Lib/Scoreboard.h
@@ -8,51 +8,37 @@
 #define SCOREBOARD_H
 
 #include <string>
-#include "wx/wx.h"
-#include "pch.h"
+#include <wx/graphics.h>
 
 class Scoreboard {
 private:
- // Get position
  int mX;
  int mY;
-
- // Scores, incrementing and decreasing
  int mLevelScore;
  int mGameScore;
- int mGoodScoreIncrement; // Score goes up
- int mBadScoreDecrement; // Score goes down
-
- // Displays text for level goal
+ int mGoodScoreIncrement;
+ int mBadScoreDecrement;
  std::string mInstructions;
 
 public:
- // Constructor
  Scoreboard(int x, int y, int goodScore = 10, int badScore = 0);
 
- // Resets level score to zero
  void ResetLevelScore();
-
- // Adds to the level score for a correct action
  void AddGoodScore();
-
- // Removes from level score for an incorrect action
  void AddBadScore();
-
- // Updates the game score when the level is finished
  void UpdateGameScore();
-
- // Draws the scoreboard on the screen
  void Draw(wxGraphicsContext* graphics);
-
- // Sets the instructions text
  void SetInstructions(const std::string& instructions);
 
- // Getters for scores and position
  int GetLevelScore() const;
  int GetGameScore() const;
  int GetX() const;
  int GetY() const;
+
+private:
+ // Helper function to draw wrapped text.
+ void DrawWrappedText(wxGraphicsContext* graphics, const wxString& text,
+                      int x, int y, int maxWidth);
 };
 
 #endif // SCOREBOARD_H
