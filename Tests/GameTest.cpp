36798abfc3eb476ae1877678d3e799c9b100dd81
nico <robe1241@msu.edu>
2024-10-18 15:14:10 -0400

    Added GameTest file and Constructor test

---
+++ Tests/GameTest.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file GameTest.cpp
+ * @author Nicolas Roberts
+ */
+
+#include <pch.h>
+#include "gtest/gtest.h"
+#include <Game.h>
+
+using namespace std;
+
+TEST(GameTest, Construct) {
+    Game game;
+}
